3534abd8e72eb6e4a68014319f98d131fca26d9b
Dylan Compton <dylancomptontx@gmail.com>
2024-12-05 19:13:14 -0600

    vercel err fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LogIn from '../views/LogIn.vue'
+import LogIn from '@/views/LogIn.vue'
 import HomePage from '@/views/HomePage.vue'
 
 const router = createRouter({
